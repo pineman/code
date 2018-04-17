7906a5c1d16b5e414daac3b3d13c544860e41ace
João Pinheiro <joao.castropinheiro@gmail.com>
2018-04-17 22:13:22 +0100

    more socket stuff

--- learn/c/posix/socket/inet/stream_inet_client.c
+++ learn/c/posix/socket/inet/stream_inet_client.c
@@ -14,46 +14,45 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	int r;
 	int s;
-	s = socket(AF_UNIX, SOCK_DGRAM, 0);
+
+	s = socket(AF_INET, SOCK_STREAM, 0);
 	if (s == -1) mperror();
 
-	struct sockaddr_un addr;
-	memset(&addr, 0, sizeof(addr));
-	addr.sun_family = AF_UNIX;
-	socklen_t addr_size = offsetof(struct sockaddr_un, sun_path) + strlen(addr.sun_path) + 1;
-	r = bind(s, (struct sockaddr *) &addr, addr_size);
+	struct sockaddr_in dest_addr;
+	socklen_t dest_addrlen = sizeof(struct sockaddr_in);
+	dest_addr.sin_family = AF_INET;
+	dest_addr.sin_port = htons(1337);
+	r = inet_aton("127.0.0.1", &dest_addr.sin_addr);
 	if (r == -1) mperror();
-
-	struct sockaddr_un dest_addr;
-	dest_addr.sun_family = AF_UNIX;
-	char path[] = "test";
-	strcpy(dest_addr.sun_path, path);
-	socklen_t dest_addr_size = offsetof(struct sockaddr_un, sun_path) + strlen(dest_addr.sun_path) + 1;
-
+	r = connect(s, (struct sockaddr *) &dest_addr, dest_addrlen);
+	if (r == -1) mperror();
 
 	size_t bufsize = 212960;
 	char buf[bufsize];
 	for (unsigned int i = 0; i < bufsize; i++) {
 		buf[i] = 'a';
 	}
+	char buf_OK[50];
 	ssize_t send_r, recv_r;
 	struct timespec t;
 	while (1) {
-		send_r = sendto(s, (void *) buf, bufsize, 0, (struct sockaddr *) &dest_addr, dest_addr_size);
+		//send_r = send(s, (void *) buf, bufsize, 0);
+		send_r = write(s, (void *) buf, bufsize);
 		if (send_r == -1) mperror();
 		clock_gettime((clockid_t) CLOCK_REALTIME, &t);
-		printf("%d.%ld: sendto() returned %zd\n", (int) t.tv_sec, t.tv_nsec, send_r);
+		printf("%d.%ld: write() returned %zd\n", (int) t.tv_sec, t.tv_nsec, send_r);
 
-		recv_r = recv(s, (void *) buf, bufsize, 0);
+		recv_r = recv(s, (void *) buf_OK, 50, MSG_WAITALL);
 		if (recv_r == -1) mperror();
 		clock_gettime((clockid_t) CLOCK_REALTIME, &t);
-		printf("%d.%ld: recv OK returned %zd\n", (int) t.tv_sec, t.tv_nsec, recv_r);
+		printf("%d.%ld: recv() %s returned %zd\n", (int) t.tv_sec, t.tv_nsec, buf_OK, recv_r);
 	}
 
 	r = close(s);
