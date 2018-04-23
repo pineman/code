48caee08f806f02a29a26128690babc9f9877735
João Pinheiro <joao.castropinheiro@gmail.com>
2018-04-23 19:26:40 +0100

    amazing threads

--- learn/matlab/fft_test.m
+++ learn/matlab/fft_test.m
@@ -1,30 +1,21 @@
-clear; clc
+close all; clear; clc;
+
 %% Signal (approximately continuous time)
 tmin = -3;
 tmax = 3;
 dt = 1e-3;
 t = tmin : dt : tmax - dt;
-
-function y = signal(t)
-	%y = rectpuls(t);
-	y = cos(4*pi*t);
-end
 x = signal(t);
 
 %% Sampling
-fs = 1; % Sampling frequency
+fs = 10; % Sampling frequency
 Ts = 1/fs; % Sampling period
 nmin = ceil(tmin / Ts); % Number of samples from t < 0
 nmax = floor(tmax / Ts); % Number of samples from t > 0
-n = nmin:nmax; % Sample numbers [time is n*Ts]
+n = nmin:nmax-1; % Sample numbers [time is n*Ts]
 xn = signal(n*Ts); % Sample the signal at n*Ts seconds (every Ts seconds)
-N = length(n) % Number of samples
+N = length(n); % Number of samples
 
-figure;
-hold on;
-plot(t, x);
-stem(n*Ts, xn);
-%{
 %% Frequency transform
 % MATLAB's fft() computes the DFT
 df = fs/N; % Frequency bins
@@ -39,6 +30,10 @@ X_real = real(X);
 X_imag = imag(X);
 
 %% Plots
+figure('Name','Signal and Samples','NumberTitle','off');
+hold on;
+plot(t, x);
+stem(n*Ts, xn);
 figure('Name','DFT - Magnitude','NumberTitle','off');
 stem(f, X_mag);
 axis([-5 5 -0.1 0.6]);
@@ -51,9 +46,14 @@ axis([-5 5 -0.1 0.6]);
 figure('Name','DFT - Imaginary Part','NumberTitle','off');
 stem(f, X_imag);
 axis([-5 5 -0.6 0.6]);
-%figure('Name','Inverse Transform vs. Signal','NumberTitle','off');
-%hold on;
-%plot(t, x);
-%stem(n*Ts, xn);
-%stem(n*Ts, ifft(ifftshift(X))*N);
-%}
+figure('Name','Inverse Transform vs. Signal','NumberTitle','off');
+hold on;
+plot(t, x);
+stem(n*Ts, xn);
+stem(n*Ts, ifft(ifftshift(X))*N);
+
+function y = signal(t)
+	y = rectpuls(t, 1);
+	%y = heaviside(t, 1);
+	%y = cos(4*pi*t);
+end
