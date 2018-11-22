c6c338b80694f5890ef2d0a8621d18413d94cdee
João Pinheiro <joao.castropinheiro@gmail.com>
2018-11-22 00:49:04 +0000

    Add new scripts, elixir & psis project

---
+++ learn/elixir/asd.exs
@@ -0,0 +1,11 @@
+current_process = self()
+
+# Spawn an Elixir process (not an operating system one!)
+spawn_link(fn ->
+	Process.send_after current_process, {:msg, "hello world"}, 100000
+end)
+
+# Block until the message is received
+receive do
+	{:msg, contents} -> IO.puts contents
+end
