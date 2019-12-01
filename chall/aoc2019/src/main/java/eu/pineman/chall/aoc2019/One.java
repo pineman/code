b1f0321ae6feede811b44d9eb390569e51e4732b
João Pinheiro <joao.castropinheiro@gmail.com>
2019-12-01 16:46:35 +0000

    aocOne: 2. make it right

--- chall/aoc2019/src/main/java/eu/pineman/chall/aoc2019/One.java
+++ chall/aoc2019/src/main/java/eu/pineman/chall/aoc2019/One.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public abstract class One {
-    static List<Path> ONE_INPUT = Arrays.asList(
-        "input/1/input.txt"
-    ).stream().map(Paths::get).collect(Collectors.toList());
+    static List<Path> ONE_INPUT = Stream.of(
+            "input/1/input.txt"
+    ).map(Paths::get).collect(Collectors.toList());
 
     public static void main(String[] args) throws IOException {
         for (Path path : ONE_INPUT) {
@@ -21,18 +21,28 @@ public abstract class One {
     }
 
     static long partOne(Path input) throws IOException {
-        return Files.lines(input).mapToLong(line -> Long.parseLong(line) / 3 - 2).sum();
+        return Files.lines(input).mapToLong(One::calcFuel).sum();
     }
 
     static long partTwo(Path input) throws IOException {
-        return Files.lines(input).mapToLong(line -> {
-            long moduleMass = Long.parseLong(line) / 3 - 2;
-            long totalModuleMass = 0;
-            while (moduleMass > 0) {
-                totalModuleMass += moduleMass;
-                moduleMass = moduleMass / 3 - 2;
-            }
-            return totalModuleMass;
-        }).sum();
+        return Files.lines(input).mapToLong(One::calcFuelTotal).sum();
+    }
+
+    private static long calcFuel(long mass) {
+        return mass / 3 - 2;
+    }
+
+    private static long calcFuel(String mass) {
+        return calcFuel(Long.parseLong(mass));
+    }
+
+    private static long calcFuelTotal(String line) {
+        long moduleMass = calcFuel(line);
+        long totalModuleMass = 0;
+        while (moduleMass > 0) {
+            totalModuleMass += moduleMass;
+            moduleMass = calcFuel(moduleMass);
+        }
+        return totalModuleMass;
     }
 }
