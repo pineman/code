0504e82822b42d954d909bf7372f1fc26ebca0f8
João Pinheiro <joao.castropinheiro@gmail.com>
2020-02-23 18:17:07 +0000

    Add es4x node experiment

---
+++ learn/js/es4x-js/index.js
@@ -0,0 +1,11 @@
+/// <reference types="es4x" />
+// @ts-check
+
+vertx
+  .createHttpServer()
+  .requestHandler(function (req) {
+    req.response().end("Hello!");
+  })
+  .listen(8080);
+
+console.log('Server listening at: http://localhost:8080/');
