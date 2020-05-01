39f624e1d81137d26e90375753b1387dbd4ab7a9
João Pinheiro <joao.castropinheiro@gmail.com>
2020-05-01 13:27:23 +0100

    cenas

--- bin/boot_windows.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-x86_64 -drive file=/dev/sda,format=raw -m 4G -smp 3 -cpu host -machine type=pc,accel=kvm -L . -bios /usr/share/ovmf/x64/OVMF_CODE.fd
--- bin/start_win10.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo tlp ac
-sudo virsh start Windows\ 10\ Local
