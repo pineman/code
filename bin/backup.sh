d7f0a80638aced9830c4f301bca015514c7a21f6
João Pinheiro <joao.castropinheiro@gmail.com>
2020-05-10 11:41:33 +0100

    Backup script

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,22 +2,23 @@
 #set -euxo pipefail
 set -x
 
-ARCHIVE=/mnt/Archive
+BACKUP=/backup
+source $BACKUP/duplicacy_password
 
-youtube-dl -j --flat-playlist 'https://www.youtube.com/playlist?list=PLI89Mvc1S1ws6TloLOnK_ecArjdr1o8ac' | jq -r '.title' > /home/pineman/Music/playlist/nostalgia-$(date +%s).txt
-youtube-dl -j --flat-playlist 'https://www.youtube.com/playlist?list=PLI89Mvc1S1wsk07ms8dz_M8_C1ft2s6A6' | jq -r '.title' > /home/pineman/Music/playlist/getfcked-$(date +%s).txt
-find /home/pineman/Music | gzip > /home/pineman/Music/music_filelist-$(date +%s).txt.gz
-mv /home/pineman/Videos/* "$ARCHIVE/Archive/Videos/" || true
-rsync -aqzzh --delete-after /home/pineman/Documents /home/pineman/Pictures /home/pineman/Music /home/pineman/code "$ARCHIVE"
-# Downloads are not synced on purporse - its temp storage that I manually organize
+pacman -Q > /home/pineman/.pkglist-$(hostname).txt
 
-sudo rsync -aqzzh --delete-after /etc "$ARCHIVE/OS Images/"
-pacman -Qqs > /home/pineman/code/pkg/pkglist-$(hostname).txt
-pacman -Qqs > /home/pineman/.pkglist-(hostname).txt
+# Downloads folder not synced on purporse - its temp storage that I manually organize
+youtube-dl -j --flat-playlist 'https://www.youtube.com/playlist?list=PLI89Mvc1S1ws6TloLOnK_ecArjdr1o8ac' | jq -r '.title' > /home/pineman/Documents/Music/playlist/nostalgia-$(date +%d-%m-%Y).txt
+youtube-dl -j --flat-playlist 'https://www.youtube.com/playlist?list=PLI89Mvc1S1wsk07ms8dz_M8_C1ft2s6A6' | jq -r '.title' > /home/pineman/Documents/Music/playlist/getfcked-$(date +%d-%m-%Y).txt
+(cd /home
+&& sudo -E duplicacy prune -keep 3:30
+&& sudo -E duplicacy prune -keep 7:360
+&& sudo -E duplicacy backup -hash -suppress CHUNK_CACHE -suppress PACK_END)
+(cd /etc
+&& sudo -E duplicacy prune -keep 3:30
+&& sudo -E duplicacy prune -keep 7:360
+&& sudo -E duplicacy backup -hash -suppress CHUNK_CACHE -suppress PACK_END)
 
-find /mnt/Archive | gzip > /mnt/Archive/list/archive_filelist-$(date +%s).txt.gz
-find /home/pineman | gzip > /mnt/Archive/list/home_filelist-$(date +%s).txt.gz
-
-# Offsite backup of most important stuff - docs and pics
-#rclone a borg repo?
-#duplicacy?
+# Offsite backup of most important stuff - Docs
+# https://github.com/gilbertchen/duplicacy/wiki/Quick-Start
+#(cd $BACKUP/home-duplicacy && sudo duplicacy copy -to b2 ... )
--- bin/mpd-log-playing.sh
+++ bin/mpd-log-playing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-  cat <(echo -n $(date +"[%d/%m/%Y %H:%M:%S]") '') <(mpc current) >> ~/Music/mpd-playing.log
+  cat <(echo -n $(date +"[%d/%m/%Y %H:%M:%S]") '') <(mpc current) >> ~/Documents/Music/mpd-playing.log
   mpc idle player
 done
