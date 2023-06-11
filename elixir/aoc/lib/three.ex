cefb15a1e035272233612677fd270b5355c1a3ee
João Pinheiro <joao.castropinheiro@gmail.com>
2023-06-11 16:42:57 +0100

    day 3 part one

--- elixir/aoc/lib/three.ex
+++ elixir/aoc/lib/three.ex
@@ -5,11 +5,40 @@ defmodule Aoc.Three do
     IO.puts(part_two(input))
   end
 
+  defp split_rucksack(rucksack) do
+    rucksack
+    |> String.split_at(div(String.length(rucksack), 2))
+    |> Tuple.to_list()
+    |> Enum.map(&String.graphemes/1)
+  end
+
+  defp get_first_common([first, second]) do
+    first
+    |> Enum.find(&(&1 in second))
+    |> String.to_charlist()
+    |> Enum.at(0)
+  end
+
+  defp map_priority(common) do
+    if common >= ?a do
+      common - ?a + 1
+    else
+      common - ?A + 27
+    end
+  end
+
   def part_one(input) do
     input
+    |> Enum.map(fn rucksack ->
+      rucksack
+      |> split_rucksack()
+      |> get_first_common()
+      |> map_priority()
+    end)
+    |> Enum.sum()
   end
 
   def part_two(input) do
-    input
+    nil
   end
 end
