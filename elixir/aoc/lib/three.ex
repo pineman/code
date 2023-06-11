b96fd534fd72418d588c67d6b2455d12556f5f56
João Pinheiro <joao.castropinheiro@gmail.com>
2023-06-11 20:54:52 +0100

    benchmark online inspiration metaprogramming

--- elixir/aoc/lib/three.ex
+++ elixir/aoc/lib/three.ex
@@ -5,15 +5,12 @@ defmodule Aoc.Three do
     IO.puts(part_two(input))
   end
 
-  defp priority(item) do
-    if item >= ?a do
-      item - ?a + 1
-    else
-      item - ?A + 27
-    end
+  # shamelessly "inspired" by https://github.com/jzimbel/adventofcode-elixir/blob/main/lib/advent_of_code/solution/year_2022/day_03.ex
+  # awesome meta-programming, love the use of Enum.concat
+  for {item, pri} <- Enum.with_index(Enum.concat(?a..?z, ?A..?Z), 1) do
+    defp priority(unquote(item)), do: unquote(pri)
   end
 
-  # shamelessly "inspired" by https://github.com/jzimbel/adventofcode-elixir/blob/main/lib/advent_of_code/solution/year_2022/day_03.ex
   defp common_list_priority(strings) do
     strings
     |> Enum.map(&(&1 |> String.to_charlist() |> MapSet.new()))
@@ -40,6 +37,15 @@ defmodule Aoc.Three do
     |> Enum.sum()
   end
 
+  # Boring solution
+  defp common_priority(item) do
+    if item >= ?a do
+      item - ?a + 1
+    else
+      item - ?A + 27
+    end
+  end
+
   def part_one(input) do
     input
     |> Enum.map(fn rucksack ->
@@ -47,12 +53,28 @@ defmodule Aoc.Three do
       |> String.to_charlist()
       |> (&Enum.split(&1, div(length(&1), 2))).()
       |> (fn {a, b} -> Enum.find(a, &(&1 in b)) end).()
-      |> priority()
+      |> common_priority()
     end)
     |> Enum.sum()
   end
 
   def part_two(input) do
+    input
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn triple ->
+      triple
+      |> Enum.map(&String.to_charlist/1)
+      |> (fn [a, b, c] ->
+            Enum.find(a, fn x ->
+              x in b && x in c
+            end)
+          end).()
+      |> common_priority()
+    end)
+    |> Enum.sum()
+  end
+
+  def part_two_meta(input) do
     input
     |> Enum.chunk_every(3)
     |> Enum.map(fn triple ->
--- elixir/aoc/test/three_test.exs
+++ elixir/aoc/test/three_test.exs
@@ -10,20 +10,26 @@ defmodule Aoc.Three.Test do
       assert Aoc.Three.part_one(input) == 8105
       assert Aoc.Three.part_one_set(input) == 8105
 
-      # Benchee.run(%{
-      #   "normal" => fn -> Aoc.Three.part_one(input) end,
-      #   "set" => fn -> Aoc.Three.part_one_set(input) end
-      # })
+      Benchee.run(%{
+        "normal" => fn -> Aoc.Three.part_one(input) end,
+        "set" => fn -> Aoc.Three.part_one_set(input) end
+      })
     end
 
     test "part two", %{input: input} do
       assert Aoc.Three.part_two(input) == 2363
       assert Aoc.Three.part_two_set(input) == 2363
 
-      # Benchee.run(%{
-      #   "normal" => fn -> Aoc.Three.part_two(input) end,
-      #   "set" => fn -> Aoc.Three.part_two_set(input) end
-      # })
+      Benchee.run(%{
+        "normal" => fn -> Aoc.Three.part_two(input) end,
+        "normal metaprogramming priority" => fn -> Aoc.Three.part_two_meta(input) end,
+        "set" => fn -> Aoc.Three.part_two_set(input) end
+      })
+
+      # conclusions:
+      #  - pattern matching "overloading" functions are not slow whatsoever,
+      #    contrary to prediction
+      # - sets are still slower as predicted!
     end
   end
 end
