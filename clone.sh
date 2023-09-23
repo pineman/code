86511e8133e5c11d71145b15ac89af318be31610
João Pinheiro <joao.castropinheiro@gmail.com>
2023-09-23 19:06:27 +0100

    add sudoku project

--- clone.sh
+++ clone.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
-clone() {
+submodule() {
   git submodule add -f $1 $2
   cd $2
   git checkout main || git checkout master
   cd -
 }
-clone git@github.com:pineman/abra.git proj/abra
-clone git@github.com:pineman/blackjack.git proj/blackjack
-clone git@github.com:pineman/wordmorph.git proj/wordmorph
-clone git@github.com:pineman/wordfind.git proj/wordfind
-clone git@github.com:hackerschool/ledmatrix.git proj/LEDmatrix
-clone git@github.com:hackerschool/r2p.git proj/r2p
-clone git@github.com:pineman/psis proj/psis
-clone git@github.com:pineman/pineman.github.io.git proj/homepage
-clone git@github.com:pineman/services.git proj/services
-clone git@gitlab.com:pineman/ju.git proj/ju
-clone git@github.com:pineman/abra-go.git proj/abra-go
-clone git@github.com:pineman/abra_elixir.git proj/abra_elixir
-clone git@github.com:pineman/aoc.git chall/aoc
-clone git@github.com:pineman/bolt_invoices.git proj/bolt_invoices
-clone https://github.com/github/gitignore git/gitignore
+submodule git@github.com:pineman/abra.git proj/abra
+submodule git@github.com:pineman/blackjack.git proj/blackjack
+submodule git@github.com:pineman/wordmorph.git proj/wordmorph
+submodule git@github.com:pineman/wordfind.git proj/wordfind
+submodule git@github.com:hackerschool/ledmatrix.git proj/LEDmatrix
+submodule git@github.com:hackerschool/r2p.git proj/r2p
+submodule git@github.com:pineman/psis proj/psis
+submodule git@github.com:pineman/pineman.github.io.git proj/homepage
+submodule git@github.com:pineman/services.git proj/services
+submodule git@gitlab.com:pineman/ju.git proj/ju
+submodule git@github.com:pineman/abra-go.git proj/abra-go
+submodule git@github.com:pineman/abra_elixir.git proj/abra_elixir
+submodule git@github.com:pineman/aoc.git chall/aoc
+submodule git@github.com:pineman/bolt_invoices.git proj/bolt_invoices
+submodule git@github.com:pineman/su.git proj/su
+submodule https://github.com/github/gitignore git/gitignore
 git clone https://github.com/sasa1977/elixir-in-action elixir/elixir-in-action
 git clone https://github.com/sasa1977/demo_system elixir/demo_system
 git clone https://github.com/dnlserrano/elixir-workshop elixir/elixir-workshop
