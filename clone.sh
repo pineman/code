d1956dadaf8ad58f729e6407b8f6f071f9115135
João Pinheiro <joao.castropinheiro@gmail.com>
2026-01-31 20:00:50 +0000

    update stuff

--- clone.sh
+++ clone.sh
@@ -3,6 +3,7 @@ submodule() {
   git submodule add -f $1 $2
   cd $2
   git checkout main || git checkout master
+  git pull
   cd -
 }
 submodule git@github.com:pineman/abra.git proj/abra
@@ -14,14 +15,12 @@ submodule git@github.com:hackerschool/r2p.git proj/r2p
 submodule git@github.com:pineman/psis proj/psis
 submodule git@github.com:pineman/pineman.github.io.git proj/homepage
 submodule git@github.com:pineman/services.git proj/services
-submodule git@gitlab.com:pineman/ju.git proj/ju
 submodule git@github.com:pineman/abra-go.git proj/abra-go
 submodule git@github.com:pineman/abra_elixir.git proj/abra_elixir
 submodule git@github.com:pineman/aoc.git chall/aoc
 submodule git@github.com:pineman/bolt_invoices.git proj/bolt_invoices
 submodule git@github.com:pineman/su.git proj/su
 submodule git@github.com:pineman/fpt.git proj/fpt
-submodule git@github.com:anapaismartins/trip-tuner.git proj/trip-tuner
 
 submodule https://github.com/github/gitignore git/gitignore
 git clone https://github.com/sasa1977/elixir-in-action elixir/elixir-in-action
